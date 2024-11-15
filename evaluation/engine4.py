f82a329a609bb504c662d3703242b21c5b19dff2
Max Krause <krause@internet-sicherheit.de>
2024-11-15 14:48:59 +0100

    playing around

--- evaluation/engine4.py
+++ evaluation/engine4.py
@@ -24,8 +24,8 @@ class ChessEngine:
                 best_value = current_value
             if time.time() - self.start_time > self.time_limit:
                 break  # Stop if time limit reached
-
-        print(f"Best move found for  {'white' if board.turn == chess.WHITE else 'black'}: {best_value}")
+        print(f"Full move {board.fullmove_number}")
+        print(f"Best Eval found for  {'white' if board.turn == chess.WHITE else 'black'}: {best_value}")
         print(f"Positions searched: {self.positions_searched}")
         return best_move
 
@@ -115,8 +115,89 @@ class ChessEngine:
 
         return value
 
+    def count_material(self, board):
+        piece_values = {
+            chess.PAWN: 1,
+            chess.KNIGHT: 3,
+            chess.BISHOP: 3,
+            chess.ROOK: 5,
+            chess.QUEEN: 9,
+            chess.KING: 0
+        }
+        material_count = 0
+
+        for piece_type in piece_values:
+            material_count += len(board.pieces(piece_type, chess.WHITE)) * piece_values[piece_type]
+            material_count += len(board.pieces(piece_type, chess.BLACK)) * piece_values[piece_type]
+
+        return material_count
+
+
     def evaluate_piece_positioning(self, board):
-        piece_square_table = {
+        piece_square_table_opening = {
+            chess.PAWN: [
+                0,  0,  0,  0,  0,  0,  0,  0,
+                50, 50, 50, 50, 50, 50, 50, 50,
+                10, 10, 20, 30, 30, 20, 10, 10,
+                5,  5, 10, 25, 25, 10,  5,  5,
+                0,  0,  0, 20, 20,  0,  0,  0,
+                5, -5,-10,  0,  0,-10, -5,  5,
+                5, 10, 10,-20,-20, 10, 10,  5,
+                0,  0,  0,  0,  0,  0,  0,  0
+            ],
+            chess.KNIGHT: [
+                -20, -10, -5, -5, -5, -5, -10, -20,
+                -10,   0,  5, 10, 10,  5,   0, -10,
+                -5,   5, 15, 20, 20, 15,   5,  -5,
+                -5,  10, 20, 30, 30, 20,  10,  -5,
+                -5,  10, 20, 30, 30, 20,  10,  -5,
+                -5,   5, 15, 20, 20, 15,   5,  -5,
+                -10,  0,  5, 10, 10,  5,   0, -10,
+                -20, -10, -5, -5, -5, -5, -10, -20
+            ],
+            chess.BISHOP: [
+                -10, -5, -5, -5, -5, -5, -5, -10,
+                -5,  10,  0,  0,  0,  0, 10,  -5,
+                -5,  0, 10, 15, 15, 10,  0,  -5,
+                -5,  5, 15, 20, 20, 15,  5,  -5,
+                -5, 10, 10, 20, 20, 10, 10,  -5,
+                -5, 15, 10, 10, 10, 10, 15,  -5,
+                -5,  5,  0,  0,  0,  0,  5,  -5,
+                -10, -5, -5, -5, -5, -5, -5, -10,
+            ],
+            chess.ROOK: [
+                0,  0,  5, 10, 10,  5,  0,  0,
+                0,  5, 10, 15, 15, 10,  5,  0,
+                0,  5, 10, 15, 15, 10,  5,  0,
+                0,  5, 10, 15, 15, 10,  5,  0,
+                0,  5, 10, 15, 15, 10,  5,  0,
+                0,  5, 10, 15, 15, 10,  5,  0,
+                0,  0,  5, 10, 10,  5,  0,  0,
+                0,  0,  5, 10, 10,  5,  0,  0
+            ],
+            chess.QUEEN: [
+                -20, -10, -10, -5, -5, -10, -10, -20,
+                -10,   0,  0,  0,  0,   0,   0, -10,
+                -10,   0,  5,  5,  5,   5,   0, -10,
+                -5,    0,  5,  5,  5,   5,   0,  -5,
+                -5,    0,  5,  5,  5,   5,   0,  -5,
+                -10,   5,  5,  5,  5,   5,   0, -10,
+                -10,   0,  5,  0,  0,   0,   0, -10,
+                -20, -10, -10, -5, -5, -10, -10, -20
+            ],
+            chess.KING: [
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -20, -30, -30, -40, -40, -30, -30, -20,
+                -10, -20, -20, -20, -20, -20, -20, -10,
+                20,  30,  10,   0,   0,  10,  30,  20,
+                20,  40,  20,   0,   0,  20,  40,  20
+            ]
+        }
+
+        piece_square_table_middlegame = {
             chess.PAWN: [
                 0,  0,  0,  0,  0,  0,  0,  0,
                 50, 50, 50, 50, 50, 50, 50, 50,
@@ -178,13 +259,97 @@ class ChessEngine:
                 20, 30, 10,  0,  0, 10, 30, 20
             ]
         }
+
+        piece_square_table_endgame = {
+            chess.PAWN: [
+                0,   0,   0,   0,   0,   0,   0,   0,
+                5,  10,  10, -20, -20,  10,  10,   5,
+                5,  -5, -10,   0,   0, -10,  -5,   5,
+                0,   0,   0,  20,  20,   0,   0,   0,
+                5,   5,  10,  25,  25,  10,   5,   5,
+                10, 10,  20,  30,  30,  20,  10,  10,
+                50, 50,  50,  50,  50,  50,  50,  50,
+                0,   0,   0,   0,   0,   0,   0,   0
+            ],
+            chess.KNIGHT: [
+                -50, -40, -30, -30, -30, -30, -40, -50,
+                -40, -20,   0,   0,   0,   0, -20, -40,
+                -30,   0,  10,  15,  15,  10,   0, -30,
+                -30,   5,  15,  20,  20,  15,   5, -30,
+                -30,   0,  15,  20,  20,  15,   0, -30,
+                -40, -20,   0,   5,   5,   0, -20, -40,
+                -50, -40, -30, -30, -30, -30, -40, -50,
+                -50, -40, -30, -30, -30, -30, -40, -50
+            ],
+            chess.BISHOP: [
+                -20, -10, -10, -10, -10, -10, -10, -20,
+                -10,   5,   0,   0,   0,   0,   5, -10,
+                -10,  10,  10,  10,  10,  10,  10, -10,
+                -10,  10,  10,  10,  10,  10,  10, -10,
+                -10,   5,  10,  10,  10,  10,   5, -10,
+                -10,   0,   5,  10,  10,   5,   0, -10,
+                -10,   0,   0,   0,   0,   0,   0, -10,
+                -20, -10, -10, -10, -10, -10, -10, -20
+            ],
+            chess.ROOK: [
+                0,   0,   0,   5,   5,   0,   0,   0,
+                -5,   0,   0,   0,   0,   0,   0,  -5,
+                -5,   0,   0,   0,   0,   0,   0,  -5,
+                -5,   0,   0,   0,   0,   0,   0,  -5,
+                5,  10,  10,  10,  10,  10,  10,   5,
+                0,   0,   0,   5,   5,   0,   0,   0,
+                5,   5,  10,  10,  10,  10,   5,   5,
+                0,   0,   0,   0,   0,   0,   0,   0
+            ],
+            chess.QUEEN: [
+                -5,  -5,  -5,  -5,  -5,  -5,  -5,  -5,
+                0,   0,   0,   0,   0,   0,   0,   0,
+                5,   5,   5,   5,   5,   5,   5,   5,
+                5,   5,   5,   5,   5,   5,   5,   5,
+                10,  10,  10,  10,  10,  10,  10,  10,
+                10,  10,  10,  10,  10,  10,  10,  10,
+                20,  20,  20,  20,  20,  20,  20,  20,
+                0,   0,   0,   0,   0,   0,   0,   0
+            ],
+            chess.KING: [
+                20,  30,  10,   0,   0,  10,  30,  20,
+                20,  20,   0,   0,   0,   0,  20,  20,
+                -10, -20, -20, -20, -20, -20, -20, -10,
+                -20, -30, -30, -40, -40, -30, -30, -20,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30,
+                -30, -40, -40, -50, -50, -40, -40, -30
+            ]
+        }
+
+
         value = 0
-        for piece_type, table in piece_square_table.items():
-            for square in board.pieces(piece_type, chess.WHITE):
-                value += table[square]
-            for square in board.pieces(piece_type, chess.BLACK):
-                value -= table[square]
-        return value / 100 
+        is_opening = board.fullmove_number < 7
+        is_endgame =  self.count_material(board) < 20
+        if is_opening:
+            for piece_type, table in piece_square_table_opening.items():
+                for square in board.pieces(piece_type, chess.WHITE):
+                    value += table[square]
+                for square in board.pieces(piece_type, chess.BLACK):
+                    mirrored_square = 63 - square
+                    value -= table[mirrored_square]
+        elif not is_endgame:
+             for piece_type, table in piece_square_table_middlegame.items():
+                for square in board.pieces(piece_type, chess.WHITE):
+                    value += table[square]
+                for square in board.pieces(piece_type, chess.BLACK):
+                    mirrored_square = 63 - square
+                    value -= table[mirrored_square]
+        else:
+            for piece_type, table in piece_square_table_endgame.items():
+                for square in board.pieces(piece_type, chess.WHITE):
+                    value += table[square]
+                for square in board.pieces(piece_type, chess.BLACK):
+                    mirrored_square = 63 - square
+                    value -= table[mirrored_square]
+        return value / 100
+
 
     def evaluate_king_safety(self, board):
         king_pos = board.king(chess.WHITE)
